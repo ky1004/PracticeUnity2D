f14cc9171b7f7d06c565f2ee91aa886c7ae8a4cb
ky1004 <pengukim@naver.com>
2022-01-19 23:22:04 +0900

    #2 PlayerBullet
    Add players to shoot bullets

--- Practice2D_2/ShootingEx/Assets/Scripts/Player.cs
+++ Practice2D_2/ShootingEx/Assets/Scripts/Player.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float power;
+
+    /* 발사 딜레이 로직을 위한 변수 */
+    public float maxShotDelay; // 실제 딜레이(값 높으면 높을수록 총알 딜레이 간격 증가)
+    public float curShotDelay; // 한발 쏜 후의 딜레이
+
+    /* 경계 구분을 위한 변수 */
     public bool isTouchTop;
     public bool isTouchBottom;
     public bool isTouchLeft;
     public bool isTouchRight;
+
+    /* 총알 오브젝트 변수 */
+    public GameObject bulletObjA;
+    public GameObject bulletObjB;
 
     Animator anim;
 
@@ -20,8 +31,16 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        Move();
+        Fire();
+        Reload();
+    }
+
+    // Move라는 함수로 캡슐화
+    void Move()
+    {
         float h = Input.GetAxisRaw("Horizontal");
-        if((isTouchRight && h == 1) || (isTouchLeft && h == -1))
+        if ((isTouchRight && h == 1) || (isTouchLeft && h == -1))
         {
             h = 0;
         }
@@ -38,11 +57,68 @@ public class Player : MonoBehaviour
         transform.position = curPos + nextPos;
 
         // 애니메이션
-        if((Input.GetButtonDown("Horizontal")) || (Input.GetButtonUp("Vertical")))
+        if ((Input.GetButtonDown("Horizontal")) || (Input.GetButtonUp("Vertical")))
         {
             // h가 float형이니 강제 형변환해서 넘김
             anim.SetInteger("Input", (int)h);
         }
+    }
+
+    void Fire()
+    {
+        // Fire1 버튼을 누르지 않았다면, 리턴한다
+        if (!Input.GetButton("Fire1"))
+        {
+            return;
+        }
+        // 장전이 안됐을 때
+        if (curShotDelay < maxShotDelay)
+        {
+            return;
+        }
+
+        switch (power)
+        {
+            case 1:
+                /* Power One */
+                // Instantiate : 매개변수 오브젝트를 생성하는 함수
+                // 오브젝트, 위치, 방향
+                GameObject bullet = Instantiate(bulletObjA, transform.position, transform.rotation);
+                Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+                rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 2:
+                /* Power Two */
+                // Vector3.right, left 단위벡터를 더해 위치 조절
+                GameObject bulletL = Instantiate(bulletObjA, transform.position + Vector3.left * 0.1f, transform.rotation);
+                GameObject bulletR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.1f, transform.rotation);
+                Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
+                rigidL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 3:
+                GameObject bulletLL = Instantiate(bulletObjA, transform.position + Vector3.left * 0.25f, transform.rotation);
+                GameObject bulletCC = Instantiate(bulletObjB, transform.position, transform.rotation);
+                GameObject bulletRR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.25f, transform.rotation);
+                Rigidbody2D rigidLL = bulletLL.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidCC = bulletCC.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidRR = bulletRR.GetComponent<Rigidbody2D>();
+                rigidLL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidCC.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidRR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+        }
+
+        
+
+        curShotDelay = 0; // 총알 쏜 다음에는 딜레이 변수 0으로 초기화
+    }
+
+    void Reload()
+    {
+        // 딜레이 변수에 Time.deltaTime을 계속 더하여 시간 계산
+        curShotDelay += Time.deltaTime;
     }
 
     // OnTriggerEnter2D로 플래그 세우기
